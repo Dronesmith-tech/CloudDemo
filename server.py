ea7ff7b8f8c3d4a461ba49b9dcdf22a10c0897c4
flatlevel <geoff@skyworksas.com>
2016-07-26 16:48:06 -0700

    Initial commit.

---
+++ server.py
@@ -0,0 +1,57 @@
+import SocketServer
+import SimpleHTTPServer
+import requests
+import json
+
+PORT = 8080
+
+USER_EMAIL      = 'geoff@skyworksas.com'
+USER_API_KEY    = '7a7c9a8a-7216-44b7-9423-737eb8c81684'
+DRONE_NAME      = 'bobby'
+
+
+class CustomHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+    def getLiveData(self):
+        url = 'http://cloud.dronesmith.io/api/drone/' + DRONE_NAME + '/live'
+
+        headers = {
+            'user-email':       USER_EMAIL,
+            'user-key':         USER_API_KEY,
+            'Content-Type':     'application/json'
+        }
+
+        response = requests.get(url, headers=headers)
+        # print 'Status code:', response
+        #
+        jsonText = json.loads(response.text)
+        # print 'Text:', jsonText
+
+        return jsonText['telemetry']
+
+
+    def do_GET(self):
+        if self.path == '/gps':
+            self.send_response(200)
+            self.send_header('Content-type','text/json')
+            self.send_header("Access-Control-Allow-Origin", "*")
+            self.end_headers()
+
+            data = self.getLiveData()
+
+            r = {
+             'lat': data['GLOBAL_POSITION_INT']['lat'],
+             'lon': data['GLOBAL_POSITION_INT']['lon'],
+             'intensity': data['PAYLOAD']['rads']
+            }
+
+            self.wfile.write(json.dumps(r))
+            return
+        else:
+            SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+httpd = SocketServer.ThreadingTCPServer(('localhost', PORT), CustomHandler)
+
+
+print 'Listening on', PORT
+httpd.serve_forever()
